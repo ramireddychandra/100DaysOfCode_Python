1fcdc6c56fbb3d840af7fd44d65b28f955f6dfcd
Ramireddy Chandra sekhar reddy <ramireddychandu77@gmail.com>
2022-11-22 19:49:31 +0530

    Day-01

---
+++ Day-1 Printing Stages/01_printing_Statements.py
@@ -0,0 +1,19 @@
+
+# Method-I
+#  Printing the Statements on Console by print statement:
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# Method-II
+# By Single line using newline ("\n") :
+
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+
+
+# Method-III
+#  By Using Multi Line String Literal ...
+print("""Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')""")
